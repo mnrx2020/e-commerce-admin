13ef349e97224122a0effd13d6c78c53578b0896
mnrx2020 <jamesmbere@gmail.com>
2024-05-23 01:07:20 +0300

    update change

--- src/Components/ListProduct/ListProduct.jsx
+++ src/Components/ListProduct/ListProduct.jsx
@@ -1,32 +1,36 @@
-import React, { useEffect, useState } from 'react'
-import "./ListProduct.css"
-import cross_icon from "../../assets/cross_icon.png"
+import React, { useEffect, useState } from 'react';
+import "./ListProduct.css";
+import cross_icon from "../../assets/cross_icon.png";
 
 const ListProduct = () => {
+  const [allProducts, setAllProducts] = useState([]);
 
-    const [allproducts,setAllProducts] = useState([])
+  const fetchInfo = async () => {
+    const response = await fetch("https://mnrx-mern-e-commerce-backend-app-api.onrender.com/allproducts");
+    const data = await response.json();
+    const baseImageUrl = import.meta.env.VITE_APP_BACKEND_URL + '/images/';
+    const modifiedProducts = data.map(product => ({
+      ...product,
+      image: baseImageUrl + product.image
+    }));
+    setAllProducts(modifiedProducts);
+  };
 
-    const fetchInfo = async ()=>{
-        await fetch("https://mnrx-mern-e-commerce-backend-app-api.onrender.com/allproducts")
-        .then((resp)=>resp.json())
-        .then((data)=>{setAllProducts(data)})
-    }
+  useEffect(() => {
+    fetchInfo();
+  }, []);
 
-    useEffect(()=>{
-        fetchInfo()
-    },[])
-
-    const remove_product = async (id) => {
-        await fetch("https://mnrx-mern-e-commerce-backend-app-api.onrender.com/removeproduct",{
-            method:"POST",
-            headers:{
-                Accept:"application/json",
-                "Content-Type":"application/json"
-            },
-            body:JSON.stringify({id:id})
-        })
-        await fetchInfo()
-    }
+  const removeProduct = async (id) => {
+    await fetch("https://mnrx-mern-e-commerce-backend-app-api.onrender.com/removeproduct", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({ id: id })
+    });
+    await fetchInfo();
+  };
 
   return (
     <div className='list-product'>
@@ -41,21 +45,20 @@ const ListProduct = () => {
       </div>
       <div className='listproduct-allproducts'>
         <hr/>
-        {allproducts.map((product,index)=>{
-            return <><div key={index} className='listproduct-format-main listproduct-format'>
-                <img src={product.image} alt='' className='listproduct-product-icon' />
-                <p>{product.name}</p>
-                <p>${product.old_price}</p>
-                <p>${product.new_price}</p>
-                <p>{product.category}</p>
-                <img onClick={()=>{remove_product(product.id)}} className='listproduct-remove-icon' src={cross_icon} alt='' />
-            </div>
+        {allProducts.map((product, index) => (
+          <div key={index} className='listproduct-format-main listproduct-format'>
+            <img src={product.image} alt='' className='listproduct-product-icon' />
+            <p>{product.name}</p>
+            <p>${product.old_price}</p>
+            <p>${product.new_price}</p>
+            <p>{product.category}</p>
+            <img onClick={() => removeProduct(product.id)} className='listproduct-remove-icon' src={cross_icon} alt='' />
             <hr/>
-            </>
-        })}
+          </div>
+        ))}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default ListProduct
+export default ListProduct;
